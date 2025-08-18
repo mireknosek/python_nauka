70604e4b159b896f2211c8ba1a314f6d754dfe0b
Miroslaw Nosek <mirek@MacBook-Pro-Miroslaw.local>
2025-08-18 09:38:54 +0200

    pojazd_cd

--- klasy.py
+++ klasy.py
@@ -3,9 +3,13 @@ class Samochod:
         self.silnik_obiektu = silnik
         print(f"Utworzono pojazd z silnikiem: {self.silnik_obiektu}. ID obiektu to {id(self)}")
 
-    def info(self):
-        print(f"Rodzaj silnika, to: {self.silnik_obiektu}")
+    def info(self, nazwa):
+        print(f"Rodzaj silnika dla {nazwa}, to: {self.silnik_obiektu}")
+
+def info2(obj, nazwa):
+    print(f"Pojazd {nazwa} ma silnik {obj.silnik_obiektu}")
 
 pojazd1 = Samochod("benzyna")
+pojazd2 = Samochod("Diesel")
 print("===============================")
-pojazd1.info()
+print(info2(pojazd1, "Fiat"))
