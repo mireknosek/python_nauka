a949ab5d80e3c3aa8f8049a196803d3f6f40a0b1
Miroslaw Nosek <mirek@MacBook-Pro-Miroslaw.local>
2025-08-25 15:45:37 +0200

    poza przestrzenią nazw

--- przestrzen_nazw.py
+++ przestrzen_nazw.py
@@ -1,18 +1,10 @@
-zmienna_a = 10
+a = [10]
 def funkcja_A():
-    print("Funkcja_A")
-    print(f"Wewnątrz A: {zmienna_a}   ID: {id(zmienna_a)}")
+    print(f"Wewnątrz A: {a} - ID: {id(a)}")
 
 def funkcja_B():
-    global zmienna_a
-    print("Funkcja_B")
-    zmienna_a = 20
-    print(f"Wewnątrz B: {zmienna_a}   ID: {id(zmienna_a)}")
-
-#Test
-print(f"Wartośc początkowa: {zmienna_a}  ID: {id(zmienna_a)}")
+    a[0] = 20
+    print(f"Wewnątrz B: {a} - ID: {id(a)}")
 
 funkcja_A()
 funkcja_B()
-
-print(f"Wartośc końcowa: {zmienna_a}  ID: {id(zmienna_a)}")
