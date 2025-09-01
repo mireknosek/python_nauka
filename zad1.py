9292a7c8a199ca09df5db5ee20aaff41591b3303
mireknosek <mirek.nosek@gmail.com>
2025-09-01 06:52:37 +0200

    zad1_cd

--- zad1.py
+++ zad1.py
@@ -1,13 +1,18 @@
-# dodawanie
-def dodaj():
-    while True:
-        try:
-            liczba = float(input("Podaj liczbę INT: "))
-            return liczba
-            break
-        except ValueError:
-            print("Podaj liczbę !")
+#
 
-liczba1 = dodaj()
-liczba2 = dodaj()
-print(f"Suma {liczba1} + {liczba2} = {liczba1 + liczba2}")
+class Liczby:
+    def __init__(self):
+        self.count = 1
+    
+    def liczba(self):
+        while True:
+            try:
+                liczba_wpr = float(input(f"POdaj liczbę float: "))
+                self.count += 1
+                return liczba_wpr
+            except ValueError:
+                print("Zła liczba")
+
+licznik = Liczby()
+l1 = licznik.liczba()
+l2 = licznik.liczba()
