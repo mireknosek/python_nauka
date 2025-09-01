65a89616228cc79092de2988c04595895ec2df57
mireknosek <mirek.nosek@gmail.com>
2025-09-01 23:01:08 +0200

    zad1_liczby

--- zad1.py
+++ zad1.py
@@ -14,7 +14,24 @@ class Liczby:
             except ValueError:
                 print("Zła liczba")
 
-suma = Liczby()
-suma.liczba()
+liczba = Liczby()
+l1 = liczba.liczba()
+l2 = liczba.liczba()
 
-        
+print(l1, l2)
+
+
+def pobierz_liczby(ile):
+    liczby = []
+    for i in range(ile):
+        while True:
+            try:
+                l = float(input("Podaj liczbę: "))
+                liczby.append(l)
+                break
+            except ValueError:
+                print("Podaj poprwaną float!")
+    return liczby
+
+#pobierz_liczby(2)
+print(pobierz_liczby(2))
