a9a28285280edb62067cea8f305bc96fb54ad9d1
Miroslaw Nosek <mirek@MacBook-Pro-Miroslaw.local>
2025-09-03 15:42:27 +0200

    zad-sklep

--- klasy_ex.py
+++ klasy_ex.py
@@ -1,7 +1,28 @@
 class Magazyn:
+    produkty = []
+
     def __init__(self, produkt, ilosc):
         self.produkt = produkt
         self.ilosc = ilosc
+        Magazyn.produkty.append(self)
         print(f"\nDodano {self.produkt}, szt. {self.ilosc} do magazynu")
+    
+    def __str__(self):
+      return f"{self.produkt} | {self.ilosc}"
+
+    def dodaj_produkt(self, produkt, ilosc):
+       self.ilosc += ilosc
+       print(f"zwiekszono stan: {produkt} do {self.ilosc}")  
+
+    def wyswietl_magazyn(cls):
+        print("\nStan magazynu:")
+        for p in cls.produkty:
+          print(f"{p.produkt} | ilość: {p.ilosc}")      
+        
 
 p1 = Magazyn("mlotek", 20)
+p2 = Magazyn("kilof", 40)
+
+p1.dodaj_produkt("mlotek", 0)
+
+p1.wyswietl_magazyn()
